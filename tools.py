5315aa786c8198e68b60c25912574ba617308856
Ife Stephen <105988938+Ife-Stephen@users.noreply.github.com>
2025-09-12 14:53:03 +0100

    Delete tools.py

--- tools.py
+++
@@ -1,9 +0,0 @@
-# tools.py
-from langchain_core.tools import tool
-
-@tool
-def add(a: int, b: int) -> int:
-    """Add two integers and return the result."""
-    return a + b
-
-TOOLS = {"add": add}
